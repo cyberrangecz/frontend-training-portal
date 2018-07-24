f5c3060e5516c7a24e091395374327ad9da00cac
hamernik <445720@mail.muni.cz>
2018-07-24 16:57:43 +0200

    Add game level configuration inputs

--- src/app/components/designer/training-definition/level-configuration/game-level-configuration/game-level-configuration.component.ts
+++ src/app/components/designer/training-definition/level-configuration/game-level-configuration/game-level-configuration.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {GameLevel} from "../../../../../model/level/game-level";
+import {AlertTypeEnum} from "../../../../../enums/alert-type.enum";
+import {AlertService} from "../../../../../services/event-services/alert.service";
+import {Hint} from "../../../../../model/level/hint";
 
 @Component({
   selector: 'game-level-configuration',
@@ -7,9 +11,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameLevelConfigurationComponent implements OnInit {
 
-  constructor() { }
+  @Input('level') level: GameLevel;
+
+  title: string;
+  content: Blob;
+  solution: Blob;
+  maxScore: number;
+  solutionPenalty: number;
+  incorrectFlagPenalty: number;
+  flag: string;
+  estimatedDuration: number;
+  hints: Hint[];
+
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
+    this.setInitialValues();
+  }
+
+  saveChanges() {
+    if (this.validateChanges()) {
+      this.setInputValuesToLevel();
+      // TODO: call service and save level through rest
+    }
+  }
+
+  private validateChanges(): boolean {
+    let errorMessage: string = '';
+
+    if (!this.title || this.title.replace(/\s/g, '') === '') {
+      errorMessage += 'Title cannot be empty\n'
+    }
+
+    if (errorMessage !== '') {
+      this.alertService.emitAlert(AlertTypeEnum.Error, errorMessage);
+      return false;
+    }
+    return true;
+  }
+
+  private setInputValuesToLevel() {
+    this.level.title = this.title;
+    this.level.content = this.content;
+    this.level.solution  = this.solution;
+    this.level.maxScore = this.maxScore;
+    this.level.flag = this.flag;
+    this.level.solutionPenalty = this.solutionPenalty;
+    this.level.incorrectFlagPenalty = this.incorrectFlagPenalty;
+    this.level.estimatedDuration = this.estimatedDuration;
+    this.level.hints = this.hints;
+  }
+
+  private setInitialValues() {
+    this.title = this.level.title;
+    this.content = this.level.content;
+    this.solution = this.level.solution;
+    this.maxScore = 1000;
+    this.flag = this.level.flag;
+    this.solutionPenalty = this.maxScore - 1;
+    this.incorrectFlagPenalty = this.level.incorrectFlagPenalty;
+    this.estimatedDuration = this.level.estimatedDuration;
+    this.hints = this.level.hints;
   }
 
 }
