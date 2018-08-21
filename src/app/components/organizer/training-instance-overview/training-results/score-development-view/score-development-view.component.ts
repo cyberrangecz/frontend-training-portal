2f8f5c1211f5d22578154a6d26c7df8ce602f458
Hai Duong Tran <445437@mail.muni.cz>
2018-08-21 13:45:59 +0200

    Add timeline visualization to organizer training instance overview

--- src/app/components/organizer/training-instance-overview/training-results/score-development-view/score-development-view.component.ts
+++ src/app/components/organizer/training-instance-overview/training-results/score-development-view/score-development-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GAME_INFORMATION } from '../../../../shared/mocks/information.mock';
+import { EVENTS } from '../../../../shared/mocks/events.mock';
 
 @Component({
   selector: 'score-development-view',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./score-development-view.component.css']
 })
 export class ScoreDevelopmentViewComponent implements OnInit {
+
+  mockFeedbackLearnerId = 9003575;
+  mockGameData = {information: GAME_INFORMATION, events: EVENTS};
 
   constructor() { }
 
