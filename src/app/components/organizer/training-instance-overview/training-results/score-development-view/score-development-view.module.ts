732aa697983dc8d63c595789fc9705e5b4a79f3f
Hai Duong Tran <445437@mail.muni.cz>
2018-08-21 16:47:13 +0200

    Revert "Add timeline visualization to organizer training instance overview"
    This reverts commit 2f8f5c1211f5d22578154a6d26c7df8ce602f458

--- src/app/components/organizer/training-instance-overview/training-results/score-development-view/score-development-view.module.ts
+++ src/app/components/organizer/training-instance-overview/training-results/score-development-view/score-development-view.module.ts
@@ -3,14 +3,12 @@ import {CommonModule} from "@angular/common";
 import { ScoreDevelopmentViewComponent } from './score-development-view.component';
 import {ScoreDevelopmentViewRoutingModule} from "./score-development-view-routing.module";
 import {ScoreDevelopmentViewMaterialModule} from "./score-development-view-material.module";
-import { Kypo2TrainingsVisualizationOverviewLibModule } from "kypo2-trainings-visualization-overview-lib";
 
 @NgModule({
   imports: [
     CommonModule,
     ScoreDevelopmentViewRoutingModule,
-    ScoreDevelopmentViewMaterialModule,
-    Kypo2TrainingsVisualizationOverviewLibModule
+    ScoreDevelopmentViewMaterialModule
   ],
   declarations: [
   ScoreDevelopmentViewComponent
