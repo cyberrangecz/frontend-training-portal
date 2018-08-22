7e898036a284542c85ae7e173ace7a3b2f89f30e
hamernik <445720@mail.muni.cz>
2018-08-22 09:09:19 +0200

    Change label pipe for level stepper to display title instead of level order

--- src/app/pipes/level-stepper-label.pipe.ts
+++ src/app/pipes/level-stepper-label.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
  */
 @Pipe({name: 'levelStepperLabel'})
 export class LevelStepperLabelPipe implements PipeTransform {
-  transform(index: number , length: number): string {
-    return length <= 10 ? 'Level ' + index : index.toString();
+  transform(title: string, index: number , length: number): string {
+    return length <= 10 ? title : index.toString();
   }
 }
