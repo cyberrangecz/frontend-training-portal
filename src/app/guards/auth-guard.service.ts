ccb4006a41a931a03dc2deb677d3ddb7943e309e
hamernik <445720@mail.muni.cz>
2018-09-10 10:31:19 +0200

    Redirect user with role - trainee directly to trainee page instead of the landing page

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import {ActiveUserService} from "../services/active-user.service";
 import {Observable} from "rxjs/internal/Observable";
+import {UserRoleEnum} from "../enums/user-role.enum";
 
 @Injectable()
 /**
@@ -16,6 +17,9 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.userService.isAuthenticated()) {
+      if (this.userService.getActiveUser().roles.contains(UserRoleEnum.Trainee) && this.userService.getActiveUser().roles.size() === 1) {
+        this.router.navigate(['/trainee']);
+      }
       return true;
     }
     this.router.navigate(['login']);
