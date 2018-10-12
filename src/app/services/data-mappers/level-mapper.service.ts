83ec39dfc3d9d54afa665f882ececfaa17431bde
hamernik <445720@mail.muni.cz>
2018-10-12 13:34:56 +0200

    Add get level to training def getter service and prepare level mapper service

--- src/app/services/data-mappers/level-mapper.service.ts
+++ src/app/services/data-mappers/level-mapper.service.ts
@@ -1,19 +1,22 @@
 import {Injectable} from "@angular/core";
 import {AbstractLevelDTO} from "../../model/DTOs/abstractLevelDTO";
 import {AbstractLevel} from "../../model/level/abstract-level";
+import {InfoLevelDTO} from "../../model/DTOs/infoLevelDTO";
 
 @Injectable()
 export class LevelMapperService {
 
   mapLevelDTOsToLevels(levelDTOs: AbstractLevelDTO[]): AbstractLevel[] {
     const result: AbstractLevel[] = [];
-
+    levelDTOs.forEach(levelDTO => result.push(this.mapLevelDTOToLevel(levelDTO)));
     return result;
   }
 
   mapLevelDTOToLevel(levelDTO: AbstractLevelDTO): AbstractLevel {
     let result: AbstractLevel;
+    if (levelDTO) {
 
+    }
     return result;
   }
 
