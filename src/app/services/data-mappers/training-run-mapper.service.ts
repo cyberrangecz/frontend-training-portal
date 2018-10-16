2d7b3bd068997a6f75c66d314a70aba186d5ab60
hamernik <445720@mail.muni.cz>
2018-10-16 09:50:49 +0200

    Add training run mapper and update training run getter service to use it

--- src/app/services/data-mappers/training-run-mapper.service.ts
+++ src/app/services/data-mappers/training-run-mapper.service.ts
@@ -1,6 +1,43 @@
 import {Injectable} from "@angular/core";
+import {TrainingRun} from "../../model/training/training-run";
+import {TrainingRunDTO} from "../../model/DTOs/trainingRunDTO";
+import {LevelMapperService} from "./level-mapper.service";
+import {TrainingRunStateEnum} from "../../enums/training-run-state.enum";
+import {TrainingInstanceMapperService} from "./training-instance-mapper.service";
 
 @Injectable()
 export class TrainingRunMapperService {
+
+  constructor(private levelMapper: LevelMapperService,
+              private trainingInstanceMapper: TrainingInstanceMapperService) {
+  }
+
+  /**
+   * Maps training run DTOs retrieved from remote server to training run objects
+   * @param trainingRunDTOs training run DTOs retrieved from remote server
+   */
+  mapTrainingRunDTOsToTrainingRuns(trainingRunDTOs: TrainingRunDTO[]): TrainingRun[] {
+    const result: TrainingRun[] = [];
+    trainingRunDTOs.forEach(trainingRunDTO => result.push(this.mapTrainingRunDTOToTrainingRun(trainingRunDTO)));
+    return result;
+  }
+
+  /**
+   * Maps training run DTO retrieved from remote server to training run object
+   * @param trainingRunDTO training run DTO retrieved from remote server
+   */
+  mapTrainingRunDTOToTrainingRun(trainingRunDTO: TrainingRunDTO): TrainingRun {
+    const result = new TrainingRun();
+    result.id = trainingRunDTO.id;
+    result.currentLevel = this.levelMapper.mapLevelDTOToLevel(trainingRunDTO.currentLevel);
+    result.startTime = trainingRunDTO.startTime;
+    result.endTime = trainingRunDTO.endTime;
+    result.eventLogReference = trainingRunDTO.eventLogReference;
+    result.sandboxInstanceId = trainingRunDTO.sandboxInstanceRef.id;
+    result.state = TrainingRunStateEnum[trainingRunDTO.state];
+    result.trainingInstance = this.trainingInstanceMapper.mapTrainingInstanceDTOToTrainingInstance(trainingRunDTO.trainingInstance);
+    return result;
+  }
+
 
 }
