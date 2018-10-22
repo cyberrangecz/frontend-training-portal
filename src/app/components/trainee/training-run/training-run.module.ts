3293548e76cb3d52c73868e33e5ebb2db98385c5
hamernik <445720@mail.muni.cz>
2018-10-22 14:14:37 +0200

    Add configuration of OIDC at the start of the application

--- src/app/components/trainee/training-run/training-run.module.ts
+++ src/app/components/trainee/training-run/training-run.module.ts
@@ -10,6 +10,9 @@ import { TrainingTimerComponent } from './training-timer/training-timer.componen
 import {CountdownTimerModule} from "ngx-countdown-timer";
 import {TrainingDefinitionGetterService} from "../../../services/data-getters/training-definition-getter.service";
 import {TrainingRunLevelsGuard} from "../../../guards/training-run-levels-guard.service";
+import {TrainingDefinitionMapperService} from "../../../services/data-mappers/training-definition-mapper.service";
+import {LevelMapperService} from "../../../services/data-mappers/level-mapper.service";
+import {TrainingRunMapperService} from "../../../services/data-mappers/training-run-mapper.service";
 
 @NgModule({
   imports: [
@@ -27,7 +30,10 @@ import {TrainingRunLevelsGuard} from "../../../guards/training-run-levels-guard.
   providers: [
     ActiveTrainingRunLevelsService,
     TrainingDefinitionGetterService,
-    TrainingRunLevelsGuard
+    TrainingDefinitionMapperService,
+    TrainingRunMapperService,
+    LevelMapperService,
+    TrainingRunLevelsGuard,
   ]
 })
 export class TrainingRunModule { }
