b16b0529ee1523058003bfda32fcacb651005bc9
Martin Hamerník <445720@mail.muni.cz>
2019-01-18 16:17:59 +0100

    Implement alert of queue to display alerts one by one

--- src/app/components/shared/alert/alert.component.ts
+++ src/app/components/shared/alert/alert.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AlertEvent} from "../../../model/events/alert-event";
 import {AlertService} from "../../../services/event-services/alert.service";
-import {MatSnackBar} from "@angular/material";
+import {MatSnackBar, MatSnackBarRef} from '@angular/material';
 import {AlertSnackbarComponent} from "./alert-snackbar/alert-snackbar.component";
 import {config} from "rxjs";
+import Queue from 'typescript-collections/dist/lib/Queue';
 
 @Component({
   selector: 'shared-alert',
@@ -13,40 +14,8 @@ import {config} from "rxjs";
 /**
  * Displays alert alert in form of Material card. Colour is derived from type of the alert and alert from alert event is displayed
  */
-export class AlertComponent implements OnInit, OnDestroy {
-
-  alertSubscription;
-
-  constructor(public snackBar: MatSnackBar,
-    private designerAlertService: AlertService) {
-    this.subscribeAlert();
-  }
+export class AlertComponent implements OnInit {
 
   ngOnInit() {
-  }
-
-  ngOnDestroy() {
-    if (this.alertSubscription) {
-      this.alertSubscription.unsubscribe();
-    }
-  }
-
-  /**
-   * Subscribes to alert events and displays them
-   */
-  private subscribeAlert() {
-    this.alertSubscription = this.designerAlertService.onAlertEventEmitted.subscribe(
-      alert => {
-        let snackBarRef = this.snackBar.openFromComponent(AlertSnackbarComponent, this.getConfig(alert));
-      }
-    )
-  }
-
-  /**
-   * Creates config for AlertSnackbarComponent
-   * @param alert received alert event
-   */
-  private getConfig(alert: AlertEvent) {
-    return alert.duration < 0 ? {data: alert} : {duration: alert.duration, data: alert};
   }
 }
