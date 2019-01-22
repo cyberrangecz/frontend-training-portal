9f8f47845b393b9e5ee8dc3c81571d36d867462f
Martin Hamerník <445720@mail.muni.cz>
2019-01-22 19:15:40 +0100

    Implement queue of alerts and sequential display of alerts

--- src/app/services/event-services/alert.service.ts
+++ src/app/services/event-services/alert.service.ts
@@ -1,29 +1,57 @@
 import {AlertEvent} from "../../model/events/alert-event";
-import {Subject} from "rxjs/internal/Subject";
-import {Observable} from "rxjs/internal/Observable";
 import {AlertTypeEnum} from "../../enums/alert-type.enum";
 import {environment} from "../../../environments/environment";
+import Queue from 'typescript-collections/dist/lib/Queue';
+import {AlertSnackbarComponent} from '../../components/shared/alert/alert-snackbar/alert-snackbar.component';
+import {MatSnackBar} from '@angular/material';
+import {Injectable} from '@angular/core';
 
 /**
  * Service for emitting and subscribing to alert events. Usually used to display result of some user action.
  */
+@Injectable()
 export class AlertService {
 
-  private _onAlertEventEmittedSubject: Subject<AlertEvent> = new Subject();
-  /**
-   * Observable emitting alert events
-   * @type {Observable<AlertEvent>} Observable of alert event
-   */
-  onAlertEventEmitted: Observable<AlertEvent> = this._onAlertEventEmittedSubject.asObservable();
+  private alertQueue: Queue<AlertEvent> = new Queue();
+
+  constructor(public snackBar: MatSnackBar) {
+
+  }
 
   /**
-   * Emits new alert
+   * Adds new alert to the queue and if its the only element in queue calls method to display it.
    * @param {AlertTypeEnum} alertType type of alert
    * @param {string} message alert to display to user
    * @param {number} duration how long should the alert be displayed. In millis, use 0 if it should be displayed until users clicks on button
    */
   emitAlert(alertType: AlertTypeEnum, message: string, duration = environment.defaultAlertDuration) {
-    this._onAlertEventEmittedSubject.next(new AlertEvent(alertType, message, duration));
+    this.alertQueue.enqueue(new AlertEvent(alertType, message, duration));
+    if (this.alertQueue.size() === 1) {
+      this.displayAlertComponent();
+    }
+  }
+
+  /**
+   * Displays alert component and binds to its subscription to remove the alert from queue on dismiss
+   */
+  private displayAlertComponent() {
+    const snackBarRef = this.snackBar.openFromComponent(AlertSnackbarComponent,
+      this.getConfig(this.alertQueue.peek()));
+
+    snackBarRef.afterDismissed().subscribe(() => {
+      this.alertQueue.dequeue();
+      if (!this.alertQueue.isEmpty()) {
+        this.displayAlertComponent();
+      }
+    });
+  }
+
+  /**
+   * Creates config for AlertSnackbarComponent
+   * @param alert received alert event
+   */
+  private getConfig(alert: AlertEvent) {
+    return alert.duration < 0 ? {data: alert} : {duration: alert.duration, data: alert};
   }
 
 }
