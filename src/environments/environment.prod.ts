55dbb4ded7a710a3b6b0c52f2b90db095c073df7
hamernik <445720@mail.muni.cz>
2019-03-08 10:57:50 +0100

    Update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,30 +6,30 @@ export const environment = {
   defaultPaginationSize: 5,
 
   // TRAINING REST API
-  trainingRestBasePath: 'http://localhost:8083/kypo2-rest-training/api/v1/',
+  trainingRestBasePath: 'http://147.251.21.216:8083/kypo2-rest-training/api/v1/',
 
   // SANDBOX REST API
-  sandboxDefsEndpointUri: 'http://147.251.21.38:8443/kypo-openstack/api/v1/definitions/',
-  poolsEndpointUri: 'http://147.251.21.38:8443/kypo-openstack/api/v1/pools/',
+  sandboxDefsEndpointUri:  'http://147.251.21.216:8443/kypo-openstack/api/v1/definitions/',
+  poolsEndpointUri: 'http://147.251.21.216:8443/kypo-openstack/api/v1/pools/',
 
   //USER AND GROUP API
-  userAndGroupRestBasePath: 'http://localhost:8084/kypo2-rest-user-and-group/api/v1/',
+  userAndGroupRestBasePath: 'http://147.251.21.216:8084/kypo2-rest-user-and-group/api/v1/',
 
   //OIDC
   // Url of the Identity Provider
   issuer: 'https://oidc.ics.muni.cz/oidc/',
   // URL of the SPA to redirect the user after silent refresh
-  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshRedirectUri: window.location.origin,
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin,
   // The SPA's id. The SPA is registered with this id at the config-server
-  clientId: '18cd6765-be1a-4de4-a6c0-6adf9b9882d1',
+  clientId: '593bbf49-a82b-4f6e-9baf-c4ed488d5066',
   // set the scope for the permissions the client should request
   scope: 'openid profile email',
   sessionChecksEnabled: false,
 
   // TOPOLOGY COMPONENT CONFIG
-  topologyRestUrl: 'http://localhost:8085/kypo2-rest-topology/api/v1/sandboxes/',
+  topologyRestUrl: 'http://147.251.21.216/kypo2-rest-topology/api/v1/sandboxes/',
   sandboxName: '',
   scenarioRestUrl: '',
   decoratorsRestUrl: '',
