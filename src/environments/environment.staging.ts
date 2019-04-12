f7ae5928c6b3fa3cbeede2a94098dea71f3e570f
Martin Hamerník <445720@mail.muni.cz>
2019-04-12 16:53:54 +0200

    Update sandbox api uri in environments

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -10,8 +10,8 @@ export const environment = {
   trainingRestBasePath: 'http://localhost:8083/kypo2-rest-training/api/v1/',
 
   // SANDBOX REST API
-  sandboxDefsEndpointUri: 'http://147.251.21.38:8443/kypo-openstack/api/v1/definitions/',
-  poolsEndpointUri: 'http://147.251.21.38:8443/kypo-openstack/api/v1/pools/',
+  sandboxDefsEndpointUri:  'http://147.251.21.216:8080/kypo-openstack/api/v1/definitions/',
+  poolsEndpointUri: 'http://147.251.21.216:8080/kypo-openstack/api/v1/pools/',
 
 
   //USER AND GROUP API
