47fc2adc82ecad5d0bcc534e06f0201782c4ff32
hamernik <445720@mail.muni.cz>
2019-05-15 12:20:52 +0200

    Add go back button to toolbar

--- src/app/components/shared/toolbar/toolbar.component.ts
+++ src/app/components/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Location } from '@angular/common';
+import {SubscriptionLike} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'shared-toolbar',
@@ -8,11 +11,35 @@ import { Component, OnInit } from '@angular/core';
 /**
  * Main toolbar of the application. Shows logo and a user menu component
  */
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isHome: boolean;
+
+  private _locationChangeSubscription: SubscriptionLike;
+
+  constructor(private location: Location,
+              private router: Router) { }
 
   ngOnInit() {
+    this.resolveIsHome();
+    this._locationChangeSubscription = this.location.subscribe(locationChange => {
+      this.resolveIsHome();
+    });
+
+    this.router.events.subscribe(routerEvent => this.resolveIsHome());
+  }
+
+  ngOnDestroy(): void {
+    if (this._locationChangeSubscription && !this._locationChangeSubscription.closed)
+      this._locationChangeSubscription.unsubscribe()
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  private resolveIsHome() {
+    this.isHome = this.location.path(false).endsWith('home');
   }
 
 }
