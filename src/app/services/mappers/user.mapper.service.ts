e874c1d2860439d3f01dfacfbc61bbdca1d7a5f5
Martin Hamerník <445720@mail.muni.cz>
2019-08-13 10:40:06 +0000

    Integrate with new user and group library

--- src/app/services/mappers/user.mapper.service.ts
+++ src/app/services/mappers/user.mapper.service.ts
@@ -1,6 +1,6 @@
 import {UserRefDTO} from '../../model/DTOs/user/user-ref-dto';
 import {UserBasicDTO} from '../../model/DTOs/user/user-basic-dto';
-import {User, UserRole} from 'kypo2-auth';
+import {User} from 'kypo2-auth';
 
 export class UserMapper {
 
@@ -12,7 +12,7 @@ export class UserMapper {
   }
 
   mapUserRefDTOToUser(userDTO: UserRefDTO): User {
-    const user = new User();
+    const user = new User([]);
     user.login = userDTO.login;
     user.id = userDTO.user_ref_id;
     user.issuer = userDTO.iss;
@@ -26,12 +26,11 @@ export class UserMapper {
   }
 
   mapUserBasicDTOToOrganizerUser(userDTO: UserBasicDTO): User {
-    const user = new User();
+    const user = new User([]);
     user.login = userDTO.login;
     user.id = userDTO.user_ref_id;
     user.issuer = userDTO.iss;
     user.name = `${userDTO.given_name} ${userDTO.family_name}`;
-    user.roles.add(UserRole.TRAINING_ORGANIZER);
     return user;
   }
 
@@ -41,12 +40,11 @@ export class UserMapper {
   }
 
   mapUserInfoDTOToDesignerUser(userDTO: UserBasicDTO): User {
-    const user = new User();
+    const user = new User([]);
     user.login = userDTO.login;
     user.id = userDTO.user_ref_id;
     user.issuer = userDTO.iss;
     user.name = `${userDTO.given_name} ${userDTO.family_name}`;
-    user.roles.add(UserRole.TRAINING_DESIGNER);
     return user;
   }
 }
--- src/app/services/shared/alert.service.ts
+++ src/app/services/shared/alert.service.ts
@@ -3,7 +3,7 @@ import {AlertTypeEnum} from "../../model/enums/alert-type.enum";
 import {environment} from "../../../environments/environment";
 import Queue from 'typescript-collections/dist/lib/Queue';
 import {AlertSnackbarComponent} from '../../components/shared/alert/alert-snackbar/alert-snackbar.component';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import {MatSnackBar} from '@angular/material/snack-bar';
 import {Injectable} from '@angular/core';
 
 /**
@@ -24,10 +24,25 @@ export class AlertService {
    * @param {string} message alert to display to user
    * @param {number} duration how long should the alert be displayed. In millis, use 0 if it should be displayed until users clicks on button
    */
-  emitAlert(alertType: AlertTypeEnum, message: string, duration = environment.defaultAlertDuration) {
-    this.alertQueue.enqueue(new AlertEvent(alertType, message, duration));
+  emitAlert(alertType: AlertTypeEnum | string, message: string, duration = environment.defaultAlertDuration) {
+    let alert: AlertTypeEnum;
+    if (typeof alertType === 'string') {
+      alert = this.convertToEnum(alertType);
+    } else {
+      alert = alertType;
+    }
+    this.alertQueue.enqueue(new AlertEvent(alert, message, duration));
     if (this.alertQueue.size() === 1) {
       this.displayAlertComponent();
+    }
+  }
+
+  private convertToEnum(alertType: string): AlertTypeEnum {
+    switch (alertType.toLowerCase()) {
+      case 'warning': return AlertTypeEnum.Warning;
+      case 'error': return AlertTypeEnum.Error;
+      case 'success': return AlertTypeEnum.Success;
+      default: return AlertTypeEnum.Info
     }
   }
 
