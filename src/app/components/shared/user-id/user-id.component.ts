cc1190659166ddf038fbfe61dca5e9d2ce6c5a14
Igor <igorignac5@gmail.com>
2019-09-18 07:11:07 +0000

    Resolve "Add user icon to toolbar"

--- src/app/components/shared/user-id/user-id.component.ts
+++ src/app/components/shared/user-id/user-id.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {Kypo2AuthService, User} from 'kypo2-auth';
-import * as Identicon from 'identicon.js';
-import { sha256 } from 'js-sha256';
 import {BaseComponent} from '../../base.component';
 import {tap} from 'rxjs/operators';
 
@@ -17,11 +15,6 @@ export class UserIdComponent extends BaseComponent implements OnInit {
 
   user: User;
   avatar: string;
-  avatarOption = {
-    background: [255, 255, 255, 255],
-    margin: 0.2,
-    size: 75,
-  };
   constructor(private authService: Kypo2AuthService) {
     super();
   }
@@ -32,7 +25,7 @@ export class UserIdComponent extends BaseComponent implements OnInit {
       .pipe(
         tap(user => {
           if (user) {
-            this.avatar = new Identicon(sha256(user.login + user.issuer), this.avatarOption).toString();
+            this.avatar = this.user.picture;
           }
         })
       )
