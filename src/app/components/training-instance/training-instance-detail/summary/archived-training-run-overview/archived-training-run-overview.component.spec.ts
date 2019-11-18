910b364667bfbc1d9b79e41f604febb38b57e3fa
Roman Felinger <445559@mail.muni.cz>
2019-11-18 08:53:31 +0000

    FE actions and dialogs tested

--- src/app/components/training-instance/training-instance-detail/summary/archived-training-run-overview/archived-training-run-overview.component.spec.ts
+++ src/app/components/training-instance/training-instance-detail/summary/archived-training-run-overview/archived-training-run-overview.component.spec.ts
@@ -9,12 +9,19 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {MaterialTestingModule} from '../../../../../testing/test-utils/material-testing.module';
 import {RouterTestingModule} from '@angular/router/testing';
 import {ArchivedTrainingRunService} from '../../../../../services/shared/archived-training-run.service';
+import {TrainingRunTableAdapter} from '../../../../../model/table-adapters/training-run-table-adapter';
+import {TrainingRun} from '../../../../../model/training/training-run';
+import {User} from 'kypo2-auth';
+import {of} from 'rxjs';
+import {MatDialog} from '@angular/material';
 
 describe('ArchivedTrainingRunOverviewComponent', () => {
 
   let archivedTrainingRunServiceSpy: jasmine.SpyObj<ArchivedTrainingRunService>;
   let component: ArchivedTrainingRunOverviewComponent;
   let fixture: ComponentFixture<ArchivedTrainingRunOverviewComponent>;
+  let dialogSpy: jasmine.Spy;
+  const dialogRefSpyObj = jasmine.createSpyObj({ afterClosed : of({}), close: null });
 
   beforeEach(() => {
     archivedTrainingRunServiceSpy = jasmine.createSpyObj('ArchivedTrainingRunService', ['getAll', 'delete', 'deleteMultiple']);
@@ -32,15 +39,69 @@ describe('ArchivedTrainingRunOverviewComponent', () => {
         Kypo2TableModule
       ],
       providers: [
-        {provide: ArchivedTrainingRunService, useValue: archivedTrainingRunServiceSpy},
+        {provide: ArchivedTrainingRunService, useValue: archivedTrainingRunServiceSpy}
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ArchivedTrainingRunOverviewComponent);
     component = fixture.componentInstance;
   });
+  beforeEach(() => {
+    dialogSpy = spyOn(TestBed.get(MatDialog), 'open').and.returnValue(dialogRefSpyObj);
+  });
 
-  it('should have created the component', () => {
+  it('should be created', () => {
     expect(component).toBeDefined();
   });
+
+  it('should add row to selected rows array', () => {
+    component.rowSelection([
+      new TrainingRunTableAdapter(createMockRun('1'), false),
+    ]);
+    expect(component.selectedTrainingRuns.length).toEqual(1);
+  });
+
+  it('should add multiple rows to selected rows array', () => {
+    component.rowSelection([
+      new TrainingRunTableAdapter(createMockRun('1'), false),
+      new TrainingRunTableAdapter(createMockRun('2'), false),
+      new TrainingRunTableAdapter(createMockRun('3'), false)
+    ]);
+    expect(component.selectedTrainingRuns.length).toEqual(3);
+  });
+
+  it('should call removal dialog on table "delete" action', () => {
+    component.onArchivedTrainingRunTableAction(createTableActionEvent());
+    expect(dialogSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call removal dialog on table "delete archived" action', () => {
+    component.deleteArchivedTrainingRuns();
+    expect(dialogSpy).toHaveBeenCalledTimes(1);
+  });
+
+  function createMockRun(id) {
+    const run = new TrainingRun();
+    run.id = id;
+    run.player = new User([]);
+    run.player.name = 'name';
+    return run;
+  }
+
+  function createTableActionEvent() {
+    const event = {
+      action: {
+        label: 'delete'
+      },
+      element: {
+        trainingRun: {
+          id: '1'
+        }
+      }
+    };
+    return event;
+  }
+
+
+
 });
