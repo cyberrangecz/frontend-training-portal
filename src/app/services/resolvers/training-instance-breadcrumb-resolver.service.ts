946c1b9f129e91115b925f6ae29b39d7cdb65923
hamernik <445720@mail.muni.cz>
2020-01-15 10:30:08 +0100

    Remove code duplication in breadcrumb resolvers

--- src/app/services/resolvers/training-instance-breadcrumb-resolver.service.ts
+++ src/app/services/resolvers/training-instance-breadcrumb-resolver.service.ts
@@ -1,15 +1,13 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
-import {EMPTY, Observable, of} from 'rxjs';
-import {catchError, mergeMap, take} from 'rxjs/operators';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {EMPTY, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {
   TRAINING_INSTANCE_EDIT_PATH,
   TRAINING_INSTANCE_NEW_PATH
 } from '../../components/training-instance/training-instance-overview/paths';
-import {RouteFactory} from '../../model/routes/route-factory';
-import {TrainingInstanceApi} from '../api/training-instance-api.service';
-import {ErrorHandlerService} from '../shared/error-handler.service';
 import {TrainingInstance} from '../../model/training/training-instance';
+import {TrainingInstanceResolver} from './training-instance-resolver.service';
 
 /**
  * Router breadcrumb title provider
@@ -17,9 +15,7 @@ import {TrainingInstance} from '../../model/training/training-instance';
 @Injectable()
 export class TrainingInstanceBreadcrumbResolver implements Resolve<string> {
 
-  constructor(private trainingInstanceFacade: TrainingInstanceApi,
-              private errorHandler: ErrorHandlerService,
-              private router: Router) {
+  constructor(private trainingInstanceResolver: TrainingInstanceResolver) {
   }
 
   /**
@@ -31,27 +27,16 @@ export class TrainingInstanceBreadcrumbResolver implements Resolve<string> {
     if (state.url.endsWith(TRAINING_INSTANCE_NEW_PATH)) {
       return 'Create';
     } else if (route.paramMap.has('id')) {
-      const id = Number(route.paramMap.get('id'));
-      return this.trainingInstanceFacade.get(id)
-        .pipe(
-          take(1),
-          mergeMap(ti => ti ? of(this.getBreadcrumbFromTitle(ti, state)) : this.navigateToOverview()),
-          catchError(err => {
-            this.errorHandler.display(err, 'Training instance breadcrumbs resolver');
-            this.navigateToOverview();
-            return EMPTY;
-          }),
+      const resolved = this.trainingInstanceResolver.resolve(route, state) as Observable<TrainingInstance>;
+      return resolved.pipe(
+          map(ti => ti ? this.getBreadcrumbFromInstance(ti, state) : ''),
         );
     }
     return EMPTY;
   }
 
-  private navigateToOverview() {
-    this.router.navigate([RouteFactory.toTrainingInstanceOverview()]);
-    return EMPTY;
-  }
 
-  private getBreadcrumbFromTitle(trainingInstance: TrainingInstance, state: RouterStateSnapshot): string {
+  private getBreadcrumbFromInstance(trainingInstance: TrainingInstance, state: RouterStateSnapshot): string {
     return state.url.includes(TRAINING_INSTANCE_EDIT_PATH)
       ? `Edit ${trainingInstance.title}`
       : trainingInstance.title;
