added36ea070176ea52f58ce6ab79049ddd422c9
Martin Hamerník <445720@mail.muni.cz>
2020-02-14 09:59:49 +0000

    Resolve "Replace Protractor with Cypress"

---
+++ e2e/src/integration/spec.ts
@@ -0,0 +1,5 @@
+import {HomePage} from '../support/home.po';
+
+it('should load home page', () => {
+  HomePage.goToPage();
+});
---
+++ e2e/src/support/home.po.ts
@@ -0,0 +1,5 @@
+export class HomePage {
+  static goToPage() {
+    cy.visit('/home');
+  }
+}
