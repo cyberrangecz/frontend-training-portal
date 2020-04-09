a1ab0859441edef9558b63c15be69ac703843790
Martin Hamerník <445720@mail.muni.cz>
2020-04-09 14:05:49 +0200

    Add support for server-side-configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,79 +1,45 @@
 import {NG_ENV} from 'angular-server-side-configuration/ng-env';
 
+export const kypo2TopologyConfig = {
+    topologyRestUrl: NG_ENV.KYPO_SANDBOX_SERVICE_URL,
+    decoratorsRestUrl: NG_ENV.KYPO_DECORATORS_SERVICE_URL ? NG_ENV.KYPO_DECORATORS_SERVICE_URL : '',
+    defaultDecoratorRefreshPeriodInSeconds: NG_ENV.KYPO_DECORATORS_REFRESH_PERIOD_SECONDS ? Number(NG_ENV.KYPO_DECORATORS_REFRESH_PERIOD_SECONDS) : 3,
+    useRealTime: NG_ENV.KYPO_TOPOLOGY_DECORATORS_USE_REAL_TIME ? Boolean(NG_ENV.KYPO_TOPOLOGY_DECORATORS_USE_REAL_TIME) : false,
+    useDecorators: NG_ENV.KYPO_TOPOLOGY_USE_DECORATORS ? Boolean(NG_ENV.KYPO_TOPOLOGY_USE_DECORATORS) : false,
+  };
+
 export const environment = {
-  production: NG_ENV.production,
-  // BEHAVIOUR SETTINGS
-  defaultNotificationDuration: Number(NG_ENV.defaultAlertDuration), // 0 to display until user dismisses it
-
+  production: true,
+  defaultNotificationDuration: NG_ENV.KYPO_DEFAULT_NOTIFICATION_DURATION ? Number(NG_ENV.KYPO_DEFAULT_NOTIFICATION_DURATION) : 5000, // 0 to display until user dismisses it
   trainingAgendaConfig: {
-    pollingPeriod: 5000,
-    defaultPaginationSize: 10,
+    pollingPeriod: NG_ENV.KYPO_POLLING_PERIOD ? Number(NG_ENV.KYPO_POLLING_PERIOD) : 5000,
+    defaultPaginationSize: NG_ENV.KYPO_PAGINATION_SIZE ? Number(NG_ENV.KYPO_PAGINATION_SIZE) : 10,
     visualizationConfig: {
-      trainingBasePath: NG_ENV.trainingRestBasePath
+      trainingBasePath: NG_ENV.KYPO_TRAINING_SERVICE_URL
     },
-    kypo2TopologyConfig: {
-      topologyRestUrl: NG_ENV.topologyRestUrl,
-      decoratorsRestUrl: NG_ENV.decoratorsRestUrl,
-      defaultDecoratorRefreshPeriodInSeconds: Number(NG_ENV.defaultDecoratorRefreshPeriodInSeconds),
-      useRealTime: Boolean(NG_ENV.useRealTime),
-      useDecorators: Boolean(NG_ENV.useDecorators),
-    },
+    kypo2TopologyConfig
   },
-
   trainingApiConfig: {
-    trainingBasePath: NG_ENV.trainingRestBasePath
+    trainingBasePath: NG_ENV.KYPO_TRAINING_SERVICE_URL
   },
-
   sandboxAgendaConfig: {
-    pollingPeriod: Number(NG_ENV.pollingPeriod),
-    defaultPaginationSize: Number(NG_ENV.defaultPaginationSize),
-    kypo2TopologyConfig: {
-      topologyRestUrl: NG_ENV.topologyRestUrl,
-      decoratorsRestUrl: NG_ENV.decoratorsRestUrl,
-      defaultDecoratorRefreshPeriodInSeconds: Number(NG_ENV.defaultDecoratorRefreshPeriodInSeconds),
-      useRealTime: Boolean(NG_ENV.useRealTime),
-      useDecorators: Boolean(NG_ENV.useDecorators),
-    },
+    pollingPeriod: NG_ENV.KYPO_POLLING_PERIOD ? Number(NG_ENV.KYPO_POLLING_PERIOD) : 5000,
+    defaultPaginationSize: NG_ENV.KYPO_PAGINATION_SIZE ? Number(NG_ENV.KYPO_PAGINATION_SIZE) : 10,
+    kypo2TopologyConfig
   },
   sandboxApiConfig: {
-    sandboxRestBasePath: NG_ENV.sandboxRestBasePath
-  },
-  kypo2TopologyConfig: {
-  topologyRestUrl: NG_ENV.topologyRestUrl,
-    decoratorsRestUrl: NG_ENV.decoratorsRestUrl,
-  defaultDecoratorRefreshPeriodInSeconds: Number(NG_ENV.defaultDecoratorRefreshPeriodInSeconds),
-  useRealTime: Boolean(NG_ENV.useRealTime),
-  useDecorators: Boolean(NG_ENV.useDecorators),
+    sandboxRestBasePath: NG_ENV.KYPO_SANDBOX_SERVICE_URL
   },
   kypo2UserAndGroupConfig: {
-    userAndGroupRestBasePath: NG_ENV.userAndGroupRestBasePath,
-    defaultPaginationSize: Number(NG_ENV.defaultPaginationSize),
+    userAndGroupRestBasePath: NG_ENV.KYPO_USER_AND_GROUP_SERVICE_URL,
+    defaultPaginationSize: NG_ENV.KYPO_PAGINATION_SIZE ? Number(NG_ENV.KYPO_PAGINATION_SIZE) : 10,
   },
   kypo2AuthConfig: {
-    maxRetryAttempts: Number(NG_ENV.maxRetryAttempts),
-    guardMainPageRedirect: NG_ENV.guardMainPageRedirect,
-    guardLoginPageRedirect: NG_ENV.guardLoginPageRedirect,
-    userInfoRestUri: NG_ENV.userInfoRestUri,
-    tokenInterceptorAllowedUrls: [
-      NG_ENV.baseUrl
-    ],
-    providers: [
-      {
-        label: NG_ENV.label,
-        textColor: NG_ENV.textColor,
-        backgroundColor: NG_ENV.backgroundColor,
-        tokenRefreshTime: Number(NG_ENV.tokenRefreshTime),
-        oidcConfig: {
-          issuer: NG_ENV.issuer,
-          clientId: NG_ENV.clientId,
-          redirectUri: NG_ENV.redirectUri,
-          scope: NG_ENV.scope,
-          logoutUrl: NG_ENV.logoutUrl,
-          postLogoutRedirectUri: NG_ENV.postLogoutRedirectUri,
-          silentRefreshRedirectUri: NG_ENV.silentRefreshRedirectUri,
-          clearHashAfterLogin: Boolean(NG_ENV.clearHashAfterLogin)
-        }
-      }
-    ]
+    maxRetryAttempts: NG_ENV.KYPO_AUTH_ATTEMPTS ? Number(NG_ENV.KYPO_AUTH_ATTEMPTS) : 3,
+    guardMainPageRedirect: NG_ENV.KYPO_HOME_REDIRECT ? NG_ENV.KYPO_HOME_REDIRECT : 'home',
+    guardLoginPageRedirect: NG_ENV.KYPO_LOGIN_REDIRECT ? NG_ENV.KYPO_LOGIN_REDIRECT : 'login',
+    tokenInterceptorAllowedUrls: NG_ENV.KYPO_OIDC_TOKEN_WHITELIST ? NG_ENV.KYPO_OIDC_TOKEN_WHITELIST.split(',') : [],
+    userInfoRestUri: NG_ENV.KYPO_USER_AND_GROUP_SERVICE_URL,
+    providers: JSON.parse(NG_ENV.KYPO_OIDC_PROVIDERS)
   }
 };
