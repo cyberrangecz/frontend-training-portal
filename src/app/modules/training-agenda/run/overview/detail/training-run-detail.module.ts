8a26cc3834bba20482de6212e49afb17f7c0d63d
2440211 <244021@mail.muni.cz>
2020-08-25 12:27:52 +0200

    Added missing import to module

--- src/app/modules/training-agenda/run/overview/detail/training-run-detail.module.ts
+++ src/app/modules/training-agenda/run/overview/detail/training-run-detail.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { TrainingRunDetailGameModule } from 'kypo-training-agenda/run-detail';
 import { DynamicEnvironment } from '../../../../../../environments/dynamic-environment';
 import { TrainingRunDetailRoutingModule } from './training-run-detail-routing.module';
+import { KypoSandboxApiModule } from 'kypo-sandbox-api';
 
 @NgModule({
   imports: [
     CommonModule,
     TrainingRunDetailGameModule.forRoot(DynamicEnvironment.getConfig().trainingAgendaConfig),
+    KypoSandboxApiModule.forRoot(DynamicEnvironment.getConfig().sandboxApiConfig),
     TrainingRunDetailRoutingModule,
   ],
 })
