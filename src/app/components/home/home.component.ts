0812edb6daa6448961b7d6a13b8bf3af26ea9e3f
Martin Hamerník <445720@mail.muni.cz>
2020-11-30 11:25:54 +0100

    Remove dependency on kypo2-auth

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SentinelAuthService } from '@sentinel/auth';
+import { SentinelAuthService, UserRole } from '@sentinel/auth';
 import { SentinelBaseDirective } from '@sentinel/common';
 import { SANDBOX_DEFINITION_PATH, SANDBOX_POOL_PATH, SANDBOX_RESOURCES_PATH } from 'kypo-sandbox-agenda';
 import { TRAINING_DEFINITION_PATH, TRAINING_INSTANCE_PATH, TRAINING_RUN_PATH } from 'kypo-training-agenda';
 import { GROUP_PATH, MICROSERVICE_PATH, USER_PATH } from 'kypo-user-and-group-agenda';
-import { UserRole } from 'kypo2-auth/lib/model/user-role';
 import { takeWhile } from 'rxjs/operators';
 import { AgendaPortalLink } from '../../model/agenda-portal-link';
 import { PortalAgendaContainer } from '../../model/portal-agenda-container';
