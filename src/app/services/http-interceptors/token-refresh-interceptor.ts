5062e11981e7e685da2e604d4d79617f65e6efae
Mário Murín <xmurin@fi.muni.cz>
2024-08-08 22:52:35 +0200

    Fix redundant queries and updating header

--- src/app/services/http-interceptors/token-refresh-interceptor.ts
+++ src/app/services/http-interceptors/token-refresh-interceptor.ts
@@ -1,43 +1,76 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, skipWhile, switchMap, take } from 'rxjs';
-import { TokenRefreshService } from '../shared/token-refresh.service';
-import { catchError } from 'rxjs/operators';
+import { concatMap, EMPTY, Observable, skipWhile, switchMap, take } from 'rxjs';
+import { TokenRefreshService, TokenRefreshState } from '../shared/token-refresh.service';
+import { SentinelAuthConfig, SentinelAuthService } from '@sentinel/auth';
 
 /**
- * Intercepts http requests, in case of 401 response, refreshes the token and
- * retries the request
+ * Intercepts http requests, to validate whether the access token is up-to-date.
  *
- * If token is being refreshed at the moment, request is postponed until
- * refresh is finished
+ * The flow is as follows:
+ * 1. The token is not expired → the request is sent.
+ * 2. The token is expired → the request is sent after refreshing the token.
+ * 3. The token is already being refreshed → the request is sent after refreshing finishes.
+ * 4. The token refresh fails → the user is logged out.
+ *
+ * @note • OIDC requests are not handled, to ensure that the OIDC flow is not interrupted
+ * @note • requires {@link TokenRefreshService}
+ * @note • requires {@link SentinelAuthService}
+ * @note • requires {@link SentinelAuthConfig}
  */
 @Injectable()
 export class TokenRefreshInterceptor implements HttpInterceptor {
-  constructor(private tokenRefreshService: TokenRefreshService) {}
+  constructor(
+    private tokenRefreshService: TokenRefreshService,
+    private sentinelAuthService: SentinelAuthService,
+    private sentinelAuthConfig: SentinelAuthConfig
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return this.tokenRefreshService.isRefreshing$.pipe(
-      skipWhile((isRefreshing) => isRefreshing),
+    if (this.isOIDCRequest(req)) {
+      return next.handle(req);
+    }
+
+    return this.handleCurrentState(this.tokenRefreshService.immediateState, next, req);
+  }
+
+  private handleCurrentState(
+    state: TokenRefreshState,
+    next: HttpHandler,
+    req: HttpRequest<any>
+  ): Observable<HttpEvent<any>> {
+    switch (state) {
+      case TokenRefreshState.REFRESHING:
+        return this.waitUntilRefreshed(next, req);
+      case TokenRefreshState.OK:
+        if (this.tokenRefreshService.isTokenExpired() && this.tokenRefreshService.canRefresh()) {
+          return this.refreshAndSend(next, req);
+        }
+        return next.handle(req);
+      case TokenRefreshState.FAILED:
+        this.sentinelAuthService.logout();
+        return EMPTY;
+    }
+  }
+
+  private waitUntilRefreshed(next: HttpHandler, req: HttpRequest<any>): Observable<HttpEvent<any>> {
+    return this.tokenRefreshService.stateObservable.pipe(
+      skipWhile((state) => state === TokenRefreshState.REFRESHING),
       take(1),
-      () => {
-        return next.handle(req).pipe(
-          (_) => _,
-          catchError((error) => {
-            if (error instanceof HttpErrorResponse && error.status === 401) {
-              return this.tokenRefreshService.refreshToken().pipe(
-                switchMap((success) => {
-                  if (success) {
-                    console.log('Token refreshed, retrying request...');
-                    return next.handle(req);
-                  }
-                  throw error;
-                })
-              );
-            }
-            throw error;
-          })
-        );
-      }
+      switchMap((state) => this.handleCurrentState(state, next, this.tokenRefreshService.updateRequestToken(req)))
+    );
+  }
+
+  private refreshAndSend(next: HttpHandler, req: HttpRequest<any>): Observable<HttpEvent<any>> {
+    return this.tokenRefreshService.refreshToken().pipe(
+      take(1),
+      concatMap((state) => this.handleCurrentState(state, next, this.tokenRefreshService.updateRequestToken(req)))
+    );
+  }
+
+  private isOIDCRequest(req: HttpRequest<any>): boolean {
+    return this.sentinelAuthConfig.providers.some(
+      (provider) => provider.oidcConfig?.issuer && req.url.startsWith(provider.oidcConfig?.issuer)
     );
   }
 }
--- src/app/services/shared/token-refresh.service.ts
+++ src/app/services/shared/token-refresh.service.ts
@@ -1,52 +1,99 @@
 import { Injectable } from '@angular/core';
-import { SentinelAuthService } from '@sentinel/auth';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { BehaviorSubject, from, Observable, of, Subject } from 'rxjs';
+import { BehaviorSubject, concatMap, from, Observable, of } from 'rxjs';
+import { HttpRequest } from '@angular/common/http';
+
+export enum TokenRefreshState {
+  REFRESHING,
+  FAILED,
+  OK,
+}
 
 @Injectable({ providedIn: 'root' })
 /**
- * Service responsible for refreshing token when it expires
+ * Service responsible for refreshing access token
  *
- * Provides isRefreshing$ observable that reflects whether token is being refreshed at the moment
+ * @note • requires {@link OAuthService}
  */
 export class TokenRefreshService {
-  private _isRefreshingSubject: Subject<boolean> = new BehaviorSubject(false);
-  isRefreshing$ = this._isRefreshingSubject.asObservable();
+  private readonly TOKEN_PREFIX = 'Bearer ';
 
-  private readonly EXPIRATION_KEY = 'expires_at';
+  private stateSubject: BehaviorSubject<TokenRefreshState> = new BehaviorSubject(TokenRefreshState.OK);
 
-  constructor(private authService: SentinelAuthService, private oauthService: OAuthService) {}
+  /**
+   * @returns Observable that emits the immediate state and all subsequent states of token refreshing
+   */
+  get stateObservable(): Observable<TokenRefreshState> {
+    return this.stateSubject.asObservable();
+  }
+
+  /**
+   * @returns Immediate state of token refreshing
+   */
+  get immediateState(): TokenRefreshState {
+    return this.stateSubject.value;
+  }
+
+  constructor(private oauthService: OAuthService) {}
 
   /**
    * Refreshes token if it is expired
+   *
    * @returns Observable that emits true if token was refreshed successfully, false otherwise
    */
-  refreshToken(): Observable<boolean> {
-    if (
-      localStorage.getItem(this.EXPIRATION_KEY) &&
-      new Date().getTime().valueOf() > parseInt(localStorage.getItem(this.EXPIRATION_KEY) as string)
-    ) {
-      this.setRefreshing(true);
-
+  refreshToken(): Observable<TokenRefreshState> {
+    if (this.isTokenExpired()) {
+      this.stateSubject.next(TokenRefreshState.REFRESHING);
       return from(
-        this.oauthService.refreshToken().then(
-          () => {
-            this.setRefreshing(false);
-            return true;
-          },
-          () => {
-            this.setRefreshing(false);
-            this.authService.logout();
-            return false;
-          }
+        this.oauthService.discoveryDocumentLoaded ? Promise.resolve() : this.oauthService.loadDiscoveryDocument()
+      ).pipe(
+        concatMap(() =>
+          this.oauthService.refreshToken().then(
+            () => {
+              this.stateSubject.next(TokenRefreshState.OK);
+              return TokenRefreshState.OK;
+            },
+            () => {
+              this.stateSubject.next(TokenRefreshState.FAILED);
+              return TokenRefreshState.FAILED;
+            }
+          )
         )
       );
     }
-    this.setRefreshing(false);
-    return of(true);
+    this.stateSubject.next(TokenRefreshState.OK);
+    return of(TokenRefreshState.OK);
   }
 
-  private setRefreshing(value: boolean): void {
-    this._isRefreshingSubject.next(value);
+  /**
+   * Updates the request with a latest token
+   *
+   * @param req request to be updated
+   * @returns updated request
+   */
+  updateRequestToken(req: HttpRequest<any>): HttpRequest<any> {
+    return req.clone({
+      setHeaders: {
+        Authorization: this.TOKEN_PREFIX + this.oauthService.getAccessToken(),
+      },
+    });
+  }
+
+  /**
+   * Checks whether the expiration time of the token has passed
+   *
+   * @returns true if token is overdue, false otherwise
+   */
+  isTokenExpired(): boolean {
+    return new Date().getTime().valueOf() > this.oauthService.getAccessTokenExpiration();
+  }
+
+  /**
+   * Checks whether the token can be refreshed
+   *
+   * @returns true has refresh token and token endpoint is set, false otherwise
+   */
+  canRefresh(): boolean {
+    return !!this.oauthService.getRefreshToken() && !!this.oauthService.tokenEndpoint;
   }
 }
