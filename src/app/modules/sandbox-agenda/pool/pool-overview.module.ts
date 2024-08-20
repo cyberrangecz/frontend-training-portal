901166777fb242b767caa9ccebd7f2ba01b1988c
Mário Murín <xmurin@fi.muni.cz>
2024-08-20 14:53:24 +0200

    Provide training api for pools overview

--- src/app/modules/sandbox-agenda/pool/pool-overview.module.ts
+++ src/app/modules/sandbox-agenda/pool/pool-overview.module.ts
@@ -4,10 +4,12 @@ import { KypoSandboxApiModule } from '@muni-kypo-crp/sandbox-api';
 import { SandboxAgendaSharedProvidersModule } from '../sandbox-agenda-shared-providers.module';
 import { PoolOverviewRoutingModule } from './pool-overview-routing.module';
 import { KypoDynamicEnvironment } from '../../../../environments/kypo-dynamic-environment';
+import { KypoTrainingApiModule } from '@muni-kypo-crp/training-api';
 @NgModule({
   imports: [
     SandboxAgendaSharedProvidersModule,
     KypoSandboxApiModule.forRoot(KypoDynamicEnvironment.getConfig().sandboxApiConfig),
+    KypoTrainingApiModule.forRoot(KypoDynamicEnvironment.getConfig().trainingApiConfig),
     PoolOverviewComponentsModule.forRoot(KypoDynamicEnvironment.getConfig().sandboxAgendaConfig),
     PoolOverviewRoutingModule,
   ],
